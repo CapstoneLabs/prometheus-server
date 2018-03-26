85a1483a6da9309a8eded297b524145d74196e1c
Matt Ouille <matthew.ouille@gmail.com>
2018-03-26 13:14:55 -0500

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# prometheus-server
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: prometheus-server
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
